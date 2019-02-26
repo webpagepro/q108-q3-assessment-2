cd1f61b1b5f2e3cf37ac7f193be32c2f792e6fe3
webpagepro <samuel@webpagepro.local>
2019-02-25 23:36:23 -0700

    Refactored using FETCH instead of AXIOS for practice sake

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -24,7 +24,7 @@ class Camera extends Component {
                     <CardTitle className="title">Camera Name: {this.props.camera.name}</CardTitle>
                     <CardImg src={this.props.camera.picture} alt={this.props.camera.name} />
                     <CardText className="price">Price:&nbsp;${this.props.camera.price.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")}</CardText>
-                    <CardText className="rating">Rating<ReactStars
+                    <CardText className="rating">Rating:<ReactStars
                         count={this.props.camera.rating}
                         size={24}
                         color2={'#ffd700'} /></CardText>
